8b333079dd57aba0570b4990dd3178e64a3be4ac
filippsap <72275215+filippsap@users.noreply.github.com>
2024-03-26 13:28:08 +0300

    Update Triangle.cs

--- TestTask/Figures/Triangle.cs
+++ TestTask/Figures/Triangle.cs
@@ -31,7 +31,7 @@ public class Triangle : IFigure
         AB = ab;
         BC = bc;
         AC = ac;
-        IsValid();
+        Validation();
     }
 
     private bool CheckRectangular() =>
@@ -39,7 +39,7 @@ public class Triangle : IFigure
         || BC == Math.Sqrt(Math.Pow(AB, 2) + Math.Pow(AC, 2))
         || AC == Math.Sqrt(Math.Pow(AB, 2) + Math.Pow(BC, 2));
 
-    private void IsValid()
+    private void Validation()
     {
         if (AB >= BC + AC || BC >= AB + AC || AC >= AB + BC) throw new ArgumentOutOfRangeException("Треугольник с такими длинами не может существовать");
         if (AB <= 0 || BC <= 0 || AC <= 0) throw new ArgumentOutOfRangeException("Треугольник с такими длинами не может существовать");
